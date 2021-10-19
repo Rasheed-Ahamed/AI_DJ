52072a1e6048096b7e0210a2bb2dfc0ff59d47f3
Rasheed Ahamed <78206567+Rasheed-Ahamed@users.noreply.github.com>
2021-10-19 14:22:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload()
 {
@@ -25,6 +27,13 @@ function gotPoses(results)
     if(results.lenght > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoint[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        console.log(results);
+        scoreRightWrist = results[0].pose.keypoint[9].score;
+        console.log("scoreRightWrist = " + scoreRightWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
@@ -41,11 +50,37 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+       circle(leftWristX, leftWristY, 20);
+       InNumberleftWristY = Number(leftWristY);
+       remove_decimals = floor(InNumberleftWristY);
+       leftWristY_divide_1000 = remove_decimals/1000;
+       volume = leftWristY_divide_1000 *2;
+       document.getElementById("volume").innerHTML = "Volume = " + volume;
+       song.setVolume(volume);
+    }
+
+    if(scoreRightWrist > 0.2)
+    {
+       circle(rightWristX, rightWristY, 20);
+       InNumberrightWristY = Number(rightWristY);
+       remove_decimals = floor(InNumberrightWristY);
+       rightWristY_divide_1000 = remove_decimals/1000;
+       speed = rightWristY_divide_1000 *2;
+       document.getElementById("speed").innerHTML = "Speed = " + speed;
+       song.setSpeed(speed);
+    }
 }
 
 function play()
 {
     song.play();
     song.setVolume(1);
+    song.setSpeed(1);
     song.rate(1);
 }
